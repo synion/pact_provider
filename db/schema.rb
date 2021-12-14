ed8088a026b25d8d998bad3aa9d9bba61b6b6526
Szymon Lach <s.lach@selleo.com>
2021-12-14 18:26:27 +0100

    Add github action

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_14_173752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "api_tokens", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_api_tokens_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "api_tokens", "users"
 end
--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -9,10 +9,8 @@ end
 Pact.provider_states_for "User" do
   provider_state "a request for sign up user" do
     tear_down do
-      User.find_by(email: 'joe@doe.com').tap do |user|
-        user.api_token.delete
-        user.delete
-      end
+      User.delete_all
+      ApiToken.delete_all
     end
   end
 end
